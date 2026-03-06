e37adfaf52a7ae707b11141ea75971d638525601
Marcokasu <xdmarco.veraxd@gmail.com>
2026-03-06 17:52:11 -0600

    TERCER AVANCE LADO DEL SERVIDOR. Se implementa la función "transmitir_a_todos" que funciona como el broadcast.                                                                         Envía un mensaje a todos los clientes excepto al remitente y usa lock para garantizar thread-safety en entornos multi-hilo. (Aun sin implementar la función "eliminar_cliente") proximamente...

--- ServidorPractica2.py
+++ ServidorPractica2.py
@@ -12,6 +12,16 @@ historial = []
 # Para evitar condiciones de carrera al modificar diccionarios/listas
 lock = threading.Lock()
 
+def transmitir_a_todos(mensaje, remitente_socket=None):
+    #Envia un mensaje a todos los clientes excepto al remitente.
+    with lock:
+        for cliente_sock in list(clientes.keys()):
+            if cliente_sock != remitente_socket:
+                try:
+                    cliente_sock.send((mensaje + "\n").encode('utf-8'))
+                except:
+                    eliminar_cliente(cliente_sock)
+
 def actualizar_lista_usuarios():
     #Envia la lista actualizada de usuarios a todos los clientes.
     with lock:
